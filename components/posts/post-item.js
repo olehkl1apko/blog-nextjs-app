197fd5dd98488a21be848a5c66cdf95d3b63f81d
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-14 15:07:00 +0200

    ADD: dummy posts, fixed post-item image and link

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -13,20 +13,25 @@ function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link>
-        <a>
-          <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
-          </div>
-          <div className={classes.content}>
-            <h3>{title}</h3>
-            <time>{formattedDate}</time>
-            <p>{excerpt}</p>
-          </div>
-        </a>
+      <Link href={linkPath}>
+        <div className={classes.image}>
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
       </Link>
     </li>
   );
