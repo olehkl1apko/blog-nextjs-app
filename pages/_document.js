10c0131aaa52a54781a31efa8acee205dd7dfdd0
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-15 10:27:29 +0200

    ADD: portal  for notifications

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang="en">
         <Head />
         <body>
           <Main />
           <NextScript />
+          <div id="notifications"></div>
         </body>
       </Html>
     );
